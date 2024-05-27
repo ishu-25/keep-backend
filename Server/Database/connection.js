59432878c8898c2ae119c9987618726fe2fc3d28
ishu-25 <ishuchampaneria03.is25@gmail.com>
2024-05-27 23:22:56 +0530

    drives url

--- Server/Database/connection.js
+++ Server/Database/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb+srv://ishika25:ishika5724@keep-cluster.ehghwtu.mongodb.net/gkeep')
+mongoose.connect('mongodb+srv://ishika25:ishika5724@keep-cluster.ehghwtu.mongodb.net/?retryWrites=true&w=majority&appName=keep-cluster')
 .then(()=>{
     console.log('Connection Successful')
 }).catch((e)=>{
