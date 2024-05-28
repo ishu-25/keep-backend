cfb7592d344acfc790b1c0ed4d1d5c54764582ab
ishu-25 <ishuchampaneria03.is25@gmail.com>
2024-05-28 11:27:05 +0530

    last 2nd time

--- Server/Database/connection.js
+++ Server/Database/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost:27017/Gkeep-notes')
+mongoose.connect('mongodb://localhost:27017/Gkeep')
 .then(()=>{
     console.log('Connection Successful')
 }).catch((e)=>{
